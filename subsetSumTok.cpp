66acfca2d26bace4068178006ff82b80e084555f
ARJUN <100413585+AR-JUN-G@users.noreply.github.com>
2023-08-08 18:12:59 +0530

    Create subsetSumTok.cpp

---
+++ subsetSumTok.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h> 
+
+bool check(int index,int target,vector<int>&arr,vector<vector<int>>&dp)
+{
+    if(target==0)   return true;
+    if(index==0)  return (arr[0]==target);
+    bool pick=false;
+    if(dp[index][target]!=-1)
+    {
+        return dp[index][target];
+    }
+    if(target>=arr[index])
+    {
+        pick=check(index-1,target-arr[index],arr,dp);
+    }
+    bool notPick=check(index-1,target,arr,dp);
+    return dp[index][target]=pick||notPick;
+}
+
+bool subsetSumToK(int n, int k, vector<int> &arr) {
+    vector<vector<int>>dp(n,vector<int>(k+1,-1));
+    return check(n-1,k,arr,dp);
+}
